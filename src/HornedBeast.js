471fd57fd4718fc790590d7e6049d89a193b9e61
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-03-15 19:54:17 -0400

    updated with features 1 and 2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,41 @@
 import React from 'react'; 
 
+
 class HornedBeast extends React.Component{
+  constructor(props){
+    super(props); 
+    this.state = {
+        clicks: 0,
+        favorited: false
+    }
+  }
+
+  handleClicks = () => {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  };
+
+  hasBeenFavorited = () => {
+     this.setState({
+       favorited: true
+     })
+  }
+
   render(){
     console.log(this.props); 
+    console.log(this.state); 
     return (
           <>
             <h2>{this.props.title}</h2>
-            <img src={this.props.imageURL} alt="" title={this.props.title}></img>
+            <img src={this.props.image_url} 
+            alt="" 
+            title={this.props.title}
+            onClick={this.handleClicks}
+            ></img>
             <p>{this.props.description}</p>
+            <p>❤️{this.state.clicks}</p>
+            <br></br>
           </>
     )
   }
