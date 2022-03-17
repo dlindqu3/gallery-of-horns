c1878eedf1f8d672abcf2b4d6d3c0d466c2e1bd3
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-03-17 18:03:55 -0400

    finished lab 03

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import Header from './header.js';
 import Main from './main.js'; 
 import Footer from './footer.js'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Modal from 'react-bootstrap/Modal';
-import Image from 'react-bootstrap/Image';
 import data from './data.json';
-
+import SelectedBeast from './selectedBeast';
 
 class App extends React.Component{
   constructor(props) {
@@ -40,32 +38,19 @@ class App extends React.Component{
 
         <Main
         data={data}
-        openModal={this.openModal}
-        
+        openModal={this.openModal}    
         /> 
 
         <Footer/> 
 
-
-        <Modal
-          show={this.state.showModal}
+        <SelectedBeast
+          image_url={this.state.image_url}
+          description={this.state.description}
+          title={this.state.title}
+          showModal={this.state.showModal}
           onHide={this.hideModal}
         >
-          <Modal.Header closeButton>
-            <Modal.Title>{this.state.title}</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            {this.state.description}
-            {/* <img src={this.state.image_url}></img>  */}
-            <Image id="big_image" src={this.state.image_url} alt={this.props.description}/>
-          </Modal.Body>
-
-          {/* <Modal.Footer>
-            {this.state.description}
-          </Modal.Footer> */}
-
-        </Modal>
+        </SelectedBeast>
       </>
     );
   }
--- src/selectedBeast.js
+++ src/selectedBeast.js
@@ -1,4 +1,42 @@
+import React from 'react'; 
+import './selectedBeast.css';
+import Modal from 'react-bootstrap/Modal';
+import Image from 'react-bootstrap/Image';
 
+class SelectedBeast extends React.Component{
+  // constructor(props){
+  //   super(props); 
+  // }
+
+  //a constructor is typically just used to set state. If state is not needed, you don't need the constructor. 
+  
+
+    render(){
+      return (
+        <>
+        <Modal
+          show={this.props.showModal}
+          onHide={this.props.onHide}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            {/* <img src={this.state.image_url}></img>  */}
+            <Image id="big_image" src={this.props.image_url} alt={this.props.description}/>
+            <h1>{this.props.description}</h1>
+          </Modal.Body>
+
+        </Modal>
+
+        </>
+      )
+    }
+  }
+
+
+export default SelectedBeast;  
 
 
 
